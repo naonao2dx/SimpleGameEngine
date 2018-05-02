dc978c72f18e3101de1a0e246b7a1e1442bb3c49
naonao2dx <naonao2dx@gmail.com>
2018-05-03 03:01:46 +0900

    Refactor Primitives class

--- SimpleGameEngine/GameEngine/2D/SGPrimitives.cpp
+++ SimpleGameEngine/GameEngine/2D/SGPrimitives.cpp
@@ -14,25 +14,85 @@
 
 using namespace SimpleGameEngine;
 
-Primitives::Primitives()
-:_shape(GL_TRIANGLES)
-,_lineWidth(1.0f)
+std::shared_ptr<Primitives> Primitives::create(int shapeType)
 {
-    init();
+    std::shared_ptr<Primitives> result(new (std::nothrow) Primitives(shapeType));
+    if (result && result->init()) {
+        result->setShaderPositionAndColor();
+        return result;
+    }
+    return nullptr;
+}
+
+std::shared_ptr<Primitives> Primitives::createWithVertexColorShader(int shapeType)
+{
+    std::shared_ptr<Primitives> result(new (std::nothrow) Primitives(shapeType));
+    if (result && result->init()) {
+        result->setShaderVertexColor();
+        return result;
+    }
+    return nullptr;
+}
+
+Primitives::Primitives(int shapeType)
+: _vertex()
+, _normalizedVertex()
+, _shapeType(shapeType)
+, _lineWidth(1.0f)
+, _color(Color4F::WHITE)
+{
+    _director = Director::getInstance();
+    _shaderManager = ShaderManager::getInstance();
 }
 
 bool Primitives::init()
 {
-    Node::init();
-    setShaderPositionAndColor();
-    
     Vertex vert1 = Vertex { Vec2 { 0.0f, 0.5f}, Color4B::RED } ;
     Vertex vert2 = Vertex { Vec2 { -0.5f, -0.5f}, Color4B::GREEN } ;
     Vertex vert3 = Vertex { Vec2 { 0.5f, -0.5f}, Color4B::BLUE } ;
     std::vector<Vertex> vertex = { vert1, vert2, vert3 };
-    setVertex(vertex);
+    setNormalizedVertex(vertex);
     
     return true;
+}
+
+void Primitives::setVertex(std::vector<Vertex> &vertex)
+{
+    _vertex = vertex;
+    
+    Size designResolutionSize = _director->getDesignResolutionSize();
+    _normalizedVertex.clear();
+    
+    for (auto itr = vertex.cbegin(); itr != vertex.cend(); ++itr) {
+        Vertex normalizedVertex;
+        normalizedVertex._position.x = (itr->_position.x / designResolutionSize.width * 2) - 1.0f;
+        normalizedVertex._position.y = ((itr->_position.y / designResolutionSize.height * 2) - 1.0f) * -1.0f;
+        normalizedVertex._color = itr->_color;
+        _normalizedVertex.emplace_back(normalizedVertex);
+    }
+}
+
+void Primitives::setNormalizedVertex(std::vector<Vertex> &normalizedVertex)
+{
+    _normalizedVertex = normalizedVertex;
+    
+    Size designResolutionSize = _director->getDesignResolutionSize();
+    _vertex.clear();
+    
+    for (auto itr = normalizedVertex.cbegin(); itr != normalizedVertex.cend(); ++itr) {
+        Vertex vertex;
+        vertex._position.x = (itr->_position.x + 1.0f) / 2 * designResolutionSize.width;
+        vertex._position.y = (itr->_position.y / -1.0f + 1.0f) / 2 * designResolutionSize.height;
+        vertex._color = itr->_color;
+        _vertex.emplace_back(vertex);
+    }
+    
+}
+
+void Primitives::setShaderProgram(ShaderManager::ShaderType shaderType)
+{
+    _shaderType = shaderType;
+    _shaderProgram = _shaderManager->getShaderProgram(shaderType);
 }
 
 void Primitives::setShaderPositionAndColor()
@@ -43,11 +103,6 @@ void Primitives::setShaderPositionAndColor()
 void Primitives::setShaderVertexColor()
 {
     setShaderProgram(ShaderManager::ShaderType::VERTEX_COLOR);
-}
-
-void Primitives::setShape(GLenum shape)
-{
-    _shape = shape;
 }
 
 void Primitives::setColor(const Color4F& color)
@@ -62,11 +117,12 @@ void Primitives::setLineWidth(GLfloat lineWidth)
 
 void Primitives::draw()
 {
-    _shaderProgram->setVertex(_vertex);
-    _shaderProgram->setShape(_shape);
+    _shaderProgram->setVertex(_normalizedVertex);
+    _shaderProgram->setShape(_shapeType);
     if (_shaderType == ShaderManager::ShaderType::POSITION_AND_COLOR) {
         std::dynamic_pointer_cast<ShaderPositionAndColor>(_shaderProgram)->setColor(_color);
         std::dynamic_pointer_cast<ShaderPositionAndColor>(_shaderProgram)->setLineWidth(_lineWidth);
     }
     _shaderProgram->draw();
 }
+
--- SimpleGameEngine/GameEngine/2D/SGPrimitives.hpp
+++ SimpleGameEngine/GameEngine/2D/SGPrimitives.hpp
@@ -9,26 +9,42 @@
 #ifndef SGPrimitives_hpp
 #define SGPrimitives_hpp
 
-#include "../Base/SGNode.hpp"
+#include "../Renderer/SGShaderManager.hpp"
+#include "../Renderer/SGShaderProgram.hpp"
+#include "../Base/SGDirector.hpp"
 
 namespace SimpleGameEngine {
     class Primitives : public Node
     {
     public:
-        Primitives();
-        void setShape(GLenum shape);
+        static std::shared_ptr<Primitives> create(int shapeType);
+        static std::shared_ptr<Primitives> createWithVertexColorShader(int shapeType);
+        void setVertex(std::vector<Vertex> &vertex);
+        void setNormalizedVertex(std::vector<Vertex> &normalizedVertex);
         void setColor(const Color4F& color);
         void setLineWidth(GLfloat lineWidth);
+        void draw();
+        inline std::vector<Vertex> getVertex() const { return _vertex; };
+        inline std::vector<Vertex> getNormalizedVertex() const { return _normalizedVertex; };
+        inline Color4F getColor() const { return _color; };
+        inline GLfloat getLineWidth() const { return _lineWidth; };
+    private:
+        Primitives(int shapeType);
         void setShaderPositionAndColor();
         void setShaderVertexColor();
-    protected:
-        virtual bool init() override;
-        virtual void draw() override;
-    private:
-        GLenum _shape;
+        bool init();
+        void setShaderProgram(ShaderManager::ShaderType shaderType);
+        std::shared_ptr<Director> _director;
+        int _shapeType;
+        int _shaderType;
+        std::vector<Vertex> _vertex;
+        std::vector<Vertex> _normalizedVertex;
         GLfloat _lineWidth;
         Color4F _color;
+        std::shared_ptr<ShaderManager> _shaderManager;
+        std::shared_ptr<ShaderProgram> _shaderProgram;
     };
 }
 
 #endif /* SGPrimitives_hpp */
+
