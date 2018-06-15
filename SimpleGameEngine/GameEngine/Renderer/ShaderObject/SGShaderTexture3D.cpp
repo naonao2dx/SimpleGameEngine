fa74af890c5aec523e331a4e0263822125791bbd
naonao2dx <naonao2dx@gmail.com>
2018-06-15 13:51:29 +0900

    Draw Cube3D by using GL_TRIANGLE_STRIP to reduce Vertex

--- SimpleGameEngine/GameEngine/Renderer/ShaderObject/SGShaderTexture3D.cpp
+++ SimpleGameEngine/GameEngine/Renderer/ShaderObject/SGShaderTexture3D.cpp
@@ -32,27 +32,21 @@ void ShaderTexture3D::draw()
 //    GLfloat position[_vertex.size() * 3];
 //    GLfloat uv[_vertex.size() * 2];
 //    vertexToArray3D(_vertex, position, nullptr, uv);
-    Vertex vertex[_vertex.size()];
-    vertexToArray(_vertex, vertex);
+    Vertex vertices[_vertex.size()];
+    vertexToArray(_vertex, vertices);
     
-    const GLushort vertexIndex[] = {
-        2, 0, 3,
-        3, 0, 1,
-        
-        4, 2, 5,
-        5, 2, 3,
-        
-        6, 4, 7,
-        7, 4, 5,
-        
-        0, 6, 1,
-        1, 6, 7,
-        
-        9, 8, 4,
-        4, 8, 2,
-        
-        5, 3, 11,
-        11, 3, 10
+    const GLushort indices[] = {
+        0, 1, 2, 3,
+        3, 2,
+        2, 3, 4, 5,
+        5, 8,
+        8, 2, 9, 4,
+        4, 3,
+        3, 10, 5, 11,
+        11, 4,
+        4, 5, 6, 7,
+        7, 6,
+        6, 7, 0, 1
     };
     
     
@@ -61,10 +55,10 @@ void ShaderTexture3D::draw()
     
     glUniform1i(_unifTexture, 0);
     
-    glVertexAttribPointer(_attrPos, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid *)vertex);
-    glVertexAttribPointer(_attrUV, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid *)((GLubyte*) vertex + sizeof(Vec3) + sizeof(Color4B)));
+    glVertexAttribPointer(_attrPos, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid *)vertices);
+    glVertexAttribPointer(_attrUV, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid *)((GLubyte*) vertices + sizeof(Vec3) + sizeof(Color4B)));
     
     glUniformMatrix4fv(_unifWlp, 1, GL_FALSE, (GLfloat*) _wlp.m);
     
-    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, vertexIndex);
+    glDrawElements(GL_TRIANGLE_STRIP, 4 * 6 + 2 * 5, GL_UNSIGNED_SHORT, indices);
 }
